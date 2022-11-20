a9c63a00465be80ec248b912eaf562dd03b3d700
Aleksey Smirnov <aasmr@ya.ru>
2022-11-20 18:35:01 +0300

    Написание страницы для разметки

--- chatreader/chatreader.py
+++ chatreader/chatreader.py
@@ -1,15 +1,28 @@
+#import asynco
 from telethon import TelegramClient
 from pickle import load
+from datetime import datetime
 
-with open('login_conf', 'rb') as f:
-    load_conf = load(f)
-    f.close()
-# Use your own values from my.telegram.org
-api_id = load_conf['api_id']
-api_hash = load_conf['api_hash']
-
-username = load_conf['username']
-
-# The first parameter is the .session file name (absolute paths allowed)
-with TelegramClient(username, api_id, api_hash) as client:
-    client.loop.run_until_complete(client.send_message('me', 'Hello, myself!'))
+async def main():
+    me = await client.get_me()
+    #print(me.stringify())
+    
+    '''
+    async for dialog in client.iter_dialogs():
+        print(dialog.name, 'has ID', dialog.id)
+    '''
+    date_offset = datetime(2022, 9, 20, tzinfo=None)
+    async for message in client.iter_messages(-1001776583381, offset_date = date_offset, reverse = True):
+        print(message.id, message.text)
+if __name__ == '__main__':
+    with open('login_conf', 'rb') as f:
+        load_conf = load(f)
+        f.close()
+    # Use your own values from my.telegram.org
+    api_id = load_conf['api_id']
+    api_hash = load_conf['api_hash']
+    username = load_conf['username']
+    
+    client = TelegramClient(username, api_id, api_hash)
+    with client:
+        client.loop.run_until_complete(main())
---
+++ site/pograncontrol_site/razmetka/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RazmetkaConfig(AppConfig):
+    name = 'razmetka'
